4327baadf6d0a819bc5562d192d2db9ab679e829
melody5417 <lengningshang@126.com>
2017-12-24 22:29:37 +0800

    Create and implement SettingsRepresentable

---
+++ Cloudy/View Controllers/SettingsViewController/Protocols/SettingsRepresentable.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsRepresentable.swift
+//  Cloudy
+//
+//  Created by melody5417 on 24/12/2017.
+//  Copyright © 2017 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+protocol SettingsRepresentable {
+    
+    var text: String { get }
+    var accessoryType: UITableViewCellAccessoryType { get }
+    
+}
