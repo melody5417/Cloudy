963e60d23037155e85ab9a8e282a61af620cbaf7
yiqiwang(王一棋) <yiqiwang@tencent.com>
2017-12-26 19:25:14 +0800

    Create WeatherDayViewViewModel and integrate into WeekViewController

--- Cloudy/View Controllers/Weather View Controllers/View Models/WeekViewViewModel.swift
+++ Cloudy/View Controllers/Weather View Controllers/View Models/WeekViewViewModel.swift
@@ -14,11 +14,6 @@ struct WeekViewViewModel {
     
     let weatherData: [WeatherDayData]
     
-    // MARK: -
-    
-    private let dayFormatter = DateFormatter()
-    private let dateFormatter = DateFormatter()
-    
     // MARK: - Public Interface
     
     var numberOfSections: Int {
@@ -30,63 +25,9 @@ struct WeekViewViewModel {
     }
     
     // MARK: -
-    
-    func day(for index: Int) -> String {
-        // Fetch weather data
-        let weatherData = self.weatherData[index]
-        
-        // Configure date formatter
-        dayFormatter.dateFormat = "EEEE"
-        
-        return dayFormatter.string(from: weatherData.time
-        )
+
+    func viewModel(for index: Int) -> WeatherDayRepresentable {
+        return WeatherDayViewViewModel(weatherDayData: weatherData[index])
     }
     
-    func date(for index: Int) -> String {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-        
-        dateFormatter.dateFormat = "MMMM d"
-        
-        return dateFormatter.string(from: weatherDayData.time)
-    }
-    
-    func temperature(for index: Int) -> String {
-        let weatherDayData = weatherData[index]
-        
-        let min = format(temperature: weatherDayData.temperatureMin)
-        let max = format(temperature: weatherDayData.temperatureMax)
-        
-        return "\(min) - \(max)"
-    }
-    
-    func windSpeed(for index: Int) -> String {
-        let weatherDayData = weatherData[index]
-        let windSpeed = weatherDayData.windSpeed
-        
-        switch UserDefaults.unitsNotation() {
-        case .imperial:
-            return String(format: "%.f MPH", windSpeed)
-        case .metric:
-            return String(format: "%.f KPH", windSpeed.toKPH())
-        }
-    }
-    
-    func image(for index: Int) -> UIImage? {
-        // Fetch Weather Data
-        let weatherDayData = weatherData[index]
-        
-        return UIImage.imageForIcon(withName: weatherDayData.icon)
-    }
-    
-    // MARK: - Private Interface
-    
-    private func format(temperature: Double) -> String {
-        switch UserDefaults.temperatureNotation() {
-        case .fahrenheit:
-            return String(format: "%.0f °F", temperature)
-        case .celsius:
-            return String(format: "%.0f °C", temperature.toCelcius())
-        }
-    }
 }
